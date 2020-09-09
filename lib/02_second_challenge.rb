1c8b1ed23ada7566589ed88b06b677dc3172417f
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-09 12:39:59 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
@@ -9,3 +12,11 @@ def second_challenge
 
   
 end
+
+	it "fill out the empty hashes that are the values of the family name keys with the appropriate key/value pairs" do
+    expect(second_challenge[:montague].keys).to eq([:patriarch, :matriarch, :hero, :hero_friends])
+    expect(second_challenge[:capulet].keys).to eq([:patriarch, :matriarch, :heroine, :heroine_friends])
+    expect(second_challenge[:montague].values).to eq([{}, {}, {}, []])
+    expect(second_challenge[:capulet].values).to eq([{}, {}, {}, []])
+
+  end
